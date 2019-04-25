dbf0f62dd092e21e2d8a881818d6caa9cd03e950
duyungang <526390578@qq.com>
2019-04-26 02:06:02 +0800

    第三版:加入了生命周期函数的调试

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,11 +3,12 @@
     <div>
   <h1>我是主页，yo!</h1>
   <button type="button" @click="run()">执行run方法</button>
+  <v-life></v-life>
     </div>
 </template>
 <script>
 
-
+import life from './life.vue';
 export default {
     data:function(){
         return {
@@ -19,6 +20,9 @@ export default {
         run(){
             alert(this.msg);
         }
+    },
+    components:{
+        'v-life':life
     }
 
 }
---
+++ src/components/life.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="life">
+        <h4>{{msg}}</h4>
+
+        <button type="button" @click="show()">生命周期执行方法</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data:function(){
+        return {
+msg:'我是一个生命周期函数组件'
+        }
+    },
+    methods:{
+        show(){
+            this.msg='改变了';
+        }
+    },
+  beforeCreate(){
+      console.log("实例刚开始创建");
+  },
+  created(){
+    console.log('实例已经创建完毕');
+  },
+  beforeMount(){
+     console.log('模板编译开始！');
+  },
+  mounted(){
+     console.log('模板编译完成！');
+  },
+  beforeUpdate(){
+     console.log('数据更新开始！');
+  },
+  updated(){
+      console.log('数据更新结束');
+  },
+  beforeDestroy(){
+      console.log('实例销毁之前');
+  },
+  destroyed(){
+      console.log('实例销毁之后');
+  }
+
+    
+}
+</script>
+
+
+<style>
+
+</style>
+
+
